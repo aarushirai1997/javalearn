4a33f8b3aea17c3d3df09447e98df599ce6c3c7e
Student <aarushi1997@gmail.com>
2017-07-09 13:08:17 +0530

    four

---
+++ aar4.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in= new Scanner(System.in);
+	char n=in.next().charAt(0);
+	if((n>='A'&&n<='Z')||(n>='a'&&n<+'z'))
+	{System.out.println("alphabet");}
+	else
+	{System.out.println("not an alphabet");}
+	}
+}
